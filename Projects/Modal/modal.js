1e6bc9314488483822d2ffb0a226707c2d71b834
Konlan21 <joekonlan16@gmail.com>
2024-03-22 20:48:27 +0000

    Add trasition to modal popup

--- Projects/Modal/modal.js
+++ Projects/Modal/modal.js
@@ -1,20 +1,17 @@
-const modalBtn = document.getElementById('modal-btn');
-const modal = document.getElementById('modal');
-const closeBtn = document.getElementById('close-btn');
+const modalBtn = document.getElementById("modal-btn");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-btn");
 
+modalBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
 
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
 
-modalBtn.addEventListener('click', () => {
-    modal.style.visibility = 'visible'
-})
-
-closeBtn.addEventListener('click', () => {
-    modal.style.visibility = "hidden";
-})
-
-
-modal.addEventListener('click', (e) => {
-    if (e.target.classList.contains('modal')) {
-        modal.style.visibility = 'hidden';
-    }
-})
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+  }
+});
