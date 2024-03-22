b0524cb68b18cf6fb0d217816a1a26347b6fa27f
Konlan21 <joekonlan16@gmail.com>
2024-03-22 22:00:52 +0000

    Update nav bar styling

--- Projects/Navbar/navbar.js
+++ Projects/Navbar/navbar.js
@@ -8,9 +8,18 @@
 // });
 
 
-const menuIcon = document.querySelector('#menu-icon');
-const navLinks = document.querySelector('#nav-links');
+// const menuIcon = document.querySelector('#menu-icon');
+// const navLinks = document.querySelector('#nav-links');
+
+// menuIcon.addEventListener('click', () => {
+//     navLinks.classList.toggle('active');
+// })
+
+
+const menuIcon = document.getElementById('menu-icon');
+const navLinkis = document.querySelector('.nav-links');
+
 
 menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    navLinkis.classList.toggle('show-nav-links')
 })
