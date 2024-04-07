eaf276b89ee230c55015584a51a55fc47cb1fd33
Konlan21 <joekonlan16@gmail.com>
2024-04-07 00:10:48 +0000

    Update navbar. Add close button to switch between menu icon when nav-bar is active

--- Projects/Navbar/navbar.js
+++ Projects/Navbar/navbar.js
@@ -1,11 +1,25 @@
 const menuIcon = document.getElementById('menu-icon');
 const navList = document.getElementById('nav-list');
-
+const closeNavBtn = document.querySelector('.bx-x');
 
 
 menuIcon.addEventListener('click', () => {
     navList.classList.toggle('active');
+    if(navList.classList.contains('active')) {
+        closeNavBtn.style.display = 'block';
+        menuIcon.style.display = 'none'
+    }
 })
+
+closeNavBtn.addEventListener('click', () => {
+    navList.classList.toggle('active')
+    if(!navList.classList.contains('active')) {
+        menuIcon.style.display = 'block';
+        closeNavBtn.style.display = 'none';
+    }
+})
+
+console.log(closeNavBtn)
 
 // Modal code
 
