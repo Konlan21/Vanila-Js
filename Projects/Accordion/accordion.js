f24a30c1bcf4103a832adc7fc3ba8da165ddf79e
Konlan21 <joekonlan16@gmail.com>
2024-07-03 22:54:53 +0000

    Improve accordion

--- Projects/Accordion/accordion.js
+++ Projects/Accordion/accordion.js
@@ -4,12 +4,23 @@ var accordionItems = document.querySelectorAll(".accordion-item");
 // Loop through each accordion item
 accordionItems.forEach(function (item) {
   // Select the title and content of each item
-  // var title = item.querySelector(".accordion-title");
+  var title = item.querySelector(".accordion-title");
   var content = item.querySelector(".accordion-content");
 
-  // Add click event listener to the entire accordion item
-  item.addEventListener("click", function () {
-    // Toggle the 'active' class on the content to toggle its visibility
+  // Add click event listener to the title
+  title.addEventListener("click", function () {
+    // Close all other accordion contents
+    accordionItems.forEach(function (otherItem) {
+      var otherTitle = otherItem.querySelector(".accordion-title");
+      var otherContent = otherItem.querySelector(".accordion-content");
+      if (otherItem !== item) {
+        otherContent.classList.remove("active");
+        otherTitle.classList.remove("active");
+      }
+    });
+
+    // Toggle the 'active' class on the clicked content to toggle its visibility
     content.classList.toggle("active");
+    title.classList.toggle("active");
   });
 });
